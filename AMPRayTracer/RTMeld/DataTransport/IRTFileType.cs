979765375ede0dc8a1a82aa18a843bc3c2ba33a9
Adedayo Odesile <aodesile@tsri.com>
2018-04-16 18:22:43 -0700

    Azure COSMOS on the way

---
+++ AMPRayTracer/RTMeld/DataTransport/IRTFileType.cs
@@ -0,0 +1,18 @@
+﻿
+using RTMeld.Attributes;
+
+namespace RTMeld.DataTransport
+{
+
+    public interface IRTFileType
+    {
+        [Map("id")]
+        int Id { get; set; }
+
+        [Map("extension,ext")]
+        string Ext { get; set; }
+
+        [Map("full_name,Fullname,fullname")]
+        string FullName { get; set; }
+    }
+}
--- AMPRayTracer/RTMeld/DataTransport/IRTUserSession.cs
+++ AMPRayTracer/RTMeld/DataTransport/IRTUserSession.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using RTMeld.DataAccess;
 using RTMeld.Attributes;
 
 
